9c01a2f23a3c0edac82f917711fbd4e1674f7a55
Кирилл <125679669+voplik@users.noreply.github.com>
2025-04-07 13:05:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import './App.css'
 
 import Home from './pages/home'
+import Telegram from './lib/Telegram'
 import { useStore } from './store/store'
 import ConnectWallet from './pages/connect-wallet'
 
 import { Switch, Route } from 'wouter'
-import { TonConnectUIProvider, useTonWallet } from '@tonconnect/ui-react'
+import { TonConnectUIProvider, useTonWallet, THEME, useTonConnectUI } from '@tonconnect/ui-react'
+import { useEffect } from 'react'
 
 
 const Router = () => {
+    const [_, setTonConnectUI] = useTonConnectUI()
     const wallet = useTonWallet()
     const { config } = useStore()
+    
+    useEffect(() => {
+        const handlerThemeChanged = () => {
+            setTonConnectUI({
+                uiPreferences: {
+                    theme: Telegram.WebApp.colorScheme === 'dark' ? THEME.DARK : THEME.LIGHT,
+                }
+            })
+        }
+        Telegram.WebApp.onEvent('themeChanged', handlerThemeChanged)
+
+        return () => {
+            Telegram.WebApp.offEvent('themeChanged', handlerThemeChanged)
+        }
+    }, [])
 
     if (!config) return null
 
@@ -23,9 +41,15 @@ const Router = () => {
 
 
 export default () => {
-    console.log(`${location.origin}${location.pathname}/tonconnect-manifest.json`)
+    
     return (
-        <TonConnectUIProvider language='ru' manifestUrl={`${location.origin}${location.pathname}/tonconnect-manifest.json`}>
+        <TonConnectUIProvider 
+            language='ru'  
+            manifestUrl={`${location.origin}${location.pathname}/tonconnect-manifest.json`}
+            uiPreferences={{
+                theme: Telegram.WebApp.colorScheme === 'dark' ? THEME.DARK : THEME.LIGHT,
+            }}
+        >
             <Router />
         </TonConnectUIProvider>
     )
